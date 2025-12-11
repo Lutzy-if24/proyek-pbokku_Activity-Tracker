ce97729c9dfc0b14be89dad21b200aac0ae4b88a
Lutzy-if24 <martulutsimbolon@gmail.com>
2025-12-11 11:22:35 +0700

    Fix: Hardcode H2 config di SecurityConfigTest dan ApplicationTest

--- src/test/java/org/delcom/app/configs/SecurityConfigTest.java
+++ src/test/java/org/delcom/app/configs/SecurityConfigTest.java
@@ -8,8 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootTest
-// @AutoConfigureMockMvc <-- Kita hapus dulu baris yang bikin error
+// KITA TAMBAHKAN PROPERTI H2 DISINI AGAR TIDAK MENCARI POSTGRESQL
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.sql.init.mode=never"
+})
 class SecurityConfigTest {
 
     @Autowired
